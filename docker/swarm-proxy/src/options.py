264fc703d6400660d04d5fc4ed05a698e33708a1
Chris Elliott <ctelliott@gmail.com>
2017-11-26 11:51:54 -0500

    Updated port

--- docker/swarm-proxy/src/options.py
+++ docker/swarm-proxy/src/options.py
@@ -6,6 +6,6 @@ import docker
 def get_options():
     return {
         'server_name': os.getenv('SWARM_PROXY_SERVICE_NAME', 'swarm01'),
-        'server_port': os.getenv('SWARM_PROXY_PORT', '3000'),
+        'server_port': os.getenv('SWARM_PROXY_PORT', '4000'),
         'docker': docker.Client(base_url='unix://var/run/docker.sock'),
     }
