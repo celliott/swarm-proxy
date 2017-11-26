32c7c6c560f9ec27b6fd7eaf33c16a7275218923
Chris Elliott <ctelliott@gmail.com>
2017-11-26 18:37:18 -0500

    Reverted commands

--- docker/swarm-proxy/src/options.py
+++ docker/swarm-proxy/src/options.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python
 
 import os
-import docker
 
 def get_options():
     return {
         'server_name': os.getenv('SWARM_PROXY_SERVICE_NAME', 'swarm01'),
         'server_port': os.getenv('SWARM_PROXY_PORT', '4000'),
-        'docker': docker.Client(base_url='unix://var/run/docker.sock'),
     }
--- docker/swarm-proxy/src/server.py
+++ docker/swarm-proxy/src/server.py
@@ -2,9 +2,10 @@
 
 import os
 import json
+import flask
 import options
 import logging
-import flask
+import subprocess
 from flask import request, Response
 
 server = flask.Flask(__name__)
@@ -14,31 +15,22 @@ logging.basicConfig(level=logging.INFO, format='[%(levelname)s] %(message)s')
 @server.route("/create_service", methods=['POST'])
 def create_service():
     data = json.loads(request.data)
-    # try:
-    results = options['docker'].services.create(
-        image = data['name'],
-        name = data['name'],
-        replicas = data['replicas'],
-        restart_policy = "any",
-        networks = data['network'],
-        container_labels = data['network'],
-        env = data['env'],
-        port = data['port'],
-    )
-    return flask.jsonify({"msg": "Service Added"})
-    # except:
-    #     return flask.jsonify({"msg": "Error, Service Not Added"})
+    try:
+        cmd = "docker service create --replicas 1 --restart-condition any --name {name} --network {network} --container-label {label} -e {env} -p {port} {image}".format(**data)
+        results = subprocess.check_output(cmd, shell=True)
+        return flask.jsonify({"results":results.replace('\n','')}), 201, {'ContentType':'application/json'}
+    except:
+        return flask.jsonify({"msg": "Error, Service Not Added"}), 500, {'ContentType':'application/json'}
 
 @server.route("/rm_service", methods=['DELETE'])
 def rm_service():
     data = json.loads(request.data)
     try:
-        results = options['docker'].services.remove(
-            image = data['name'],
-        )
-        return flask.jsonify({"msg": "Service Removed"})
+        cmd = "docker service rm {name}".format(**data)
+        results = subprocess.check_output(cmd, shell=True)
+        return flask.jsonify({"results":results.replace('\n','')}), 202, {'ContentType':'application/json'}
     except:
-        return flask.jsonify({"msg": "Error, Service Not Removed"})
+        return flask.jsonify({"msg": "Error, Service Not Removed"}), 500, {'ContentType':'application/json'}
 
 if __name__ == "__main__":
   server.run(host='0.0.0.0', port=options['server_port'])
